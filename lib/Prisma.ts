f4c4614dd75649b2317341eed95492867d4de431
pacuwa <167692657+pacuwa@users.noreply.github.com>
2026-08-24 22:06:15 +0300

    Create Prisma.ts

---
+++ lib/Prisma.ts
@@ -0,0 +1,19 @@
+// Database client initialization
+import { PrismaClient } from '@prisma/client'
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // Avoid instantiating too many PrismaClient instances during development
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient
+  }
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient()
+  }
+  prisma = globalWithPrisma.prisma
+}
+
+export default prisma
