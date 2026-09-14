cd2fd4d90f272dc516daa01cb66f13635735a5c5
albertocen-prog <albertjesocen@gmail.com>
2026-09-14 14:44:53 +0300

    Create prisma. cofigure.ts

---
+++ prisma. cofigure.ts
@@ -0,0 +1,13 @@
+// prisma.config.ts
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: {
+    kind: 'single',
+    filePath: 'prisma/schema.prisma',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
