78964a07efda1ea965a027829f3e454cd791092d
albertocen-prog <albertjesocen@gmail.com>
2026-09-14 15:45:59 +0300

    Update Prisma.ts

--- lib/Prisma.ts
+++ lib/Prisma.ts
@@ -1,23 +1,10 @@
-// Database client initialization
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  })
-} else {
-  // Avoid instantiating too many PrismaClient instances during development
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient
-  }
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient({
-      datasourceUrl: process.env.DATABASE_URL,
-    })
-  }
-  prisma = globalWithPrisma.prisma
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
