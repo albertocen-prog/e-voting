6c2a97636e2a30d3ced78a26a6b64f306320b8cb
albertocen-prog <albertjesocen@gmail.com>
2026-09-17 13:57:42 +0300

    Update jwt.ts

--- lib/auth/jwt.ts
+++ lib/auth/jwt.ts
@@ -1,5 +1,9 @@
-import jwt from 'jsonwebtoken';
-import { DecodedToken } from './types';
+// If defined as 'enum Role' in schema.prisma:
+import { Role } from '@prisma/client';
+
+// Or if defined as 'enum UserRole' in schema.prisma:
+import { UserRole } from '@prisma/client';
+
 
 const JWT_SECRET = process.env.JWT_SECRET || 'default_secret';
 
