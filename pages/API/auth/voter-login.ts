cc5dbd1dd919e21b686f1dba4652cf22af640ee1
albertocen-prog <albertjesocen@gmail.com>
2026-09-17 23:30:57 +0300

    Update voter-login.ts

--- pages/API/auth/voter-login.ts
+++ pages/API/auth/voter-login.ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiResponse } from 'next';
 import { prisma } from '@/lib/db';
-import { generateToken } from '@/lib/auth/jwt';
+import { signToken } from '@/lib/auth/jwt';
 import { VoterLoginRequest, AuthResponse } from '@/lib/auth/types';
 
 /**
