ca32fdab660a942781641b1aa19ee08c75e189f9
albertocen-prog <albertjesocen@gmail.com>
2026-09-18 13:43:00 +0300

    Update [id].ts

--- pages/api/ballots/[id].ts
+++ pages/api/ballots/[id].ts
@@ -33,4 +33,4 @@ async function handler(
 }
 
 // Pass the required role string as the first argument if requireRole expects it
-export default authMiddleware('admin', handler)
+export default authMiddleware( handler)
