50881371eac1dae632df9ed991b2925bf8e45866
albertocen-prog <albertjesocen@gmail.com>
2026-09-18 14:02:46 +0300

    Update my-receipt.ts

--- pages/api/elections/[electionId]/my-receipt.ts
+++ pages/api/elections/[electionId]/my-receipt.ts
@@ -33,7 +33,7 @@ const handler = async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
     // Get the vote for this voter in this election
     const vote = await prisma.vote.findUnique({
       where: {
-        election_id_voterRegistrationId: {
+        election_id: {
           election_id: electionId,
           voterRegistrationId: voterRegistration.id,
         },
