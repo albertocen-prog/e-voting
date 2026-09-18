ad00af96d5950b7751e70d4ca487404760bb7a3d
albertocen-prog <albertjesocen@gmail.com>
2026-09-18 10:34:38 +0300

    Update logout.ts

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,4 +1,4 @@
-import type { NextApiResponse, NextApiRequest} from 'next';
+import type { NextApiResponse } from 'next';
 import { NextApiRequestWithAuth, authMiddleware } from '@/lib/auth/middleware';
 import { prisma } from '@/lib/db';
 
