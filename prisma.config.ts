4753cb832fe74f2118389b579ce380c066e4a0cd
albertocen-prog <albertjesocen@gmail.com>
2026-09-19 10:20:12 +0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,1 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "node prisma/seed.js",
-  },
-  datasource: {
-    // Provide a dummy fallback value if DATABASE_URL is missing during build time
-    url: env("DATABASE_URL") ?? "postgresql://dummy:dummy@localhost:5432/dummy",
-  },
-});
