5df02fc07ea84bb7428a6fe51fb450e576e463bd
albertocen-prog <albertjesocen@gmail.com>
2026-09-19 23:40:57 +0300

    Update index.ts

--- pages/api/votes/index.ts
+++ pages/api/votes/index.ts
@@ -80,7 +80,7 @@ async function handler(req: AuthenticatedRequest, res: NextApiResponse) {
         await tx.ballotParticipation.create({
           data: {
             ballotId,
-            voterRegistrationId: voterRegId,
+            voterId: voterRegId,
           },
         })
 
