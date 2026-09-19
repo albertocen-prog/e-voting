7b80d128635a216a704f461f3d24804b45408140
albertocen-prog <albertjesocen@gmail.com>
2026-09-19 12:46:54 +0300

    Update index.ts

--- pages/api/votes/index.ts
+++ pages/api/votes/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { requireApprovedVoter } from '@/lib/auth/middleware'
-import { prisma, Type prisma} from '@/lib/db'
+import { prisma, type Prisma } from '@/lib/db'
 
 export interface AuthUser {
   userId: string
