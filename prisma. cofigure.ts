8ed9b354c23ff4fc87016bbec543016ad2b3adde
albertocen-prog <albertjesocen@gmail.com>
2026-09-19 09:45:53 +0300

    Update prisma. cofigure.ts

--- prisma. cofigure.ts
+++ prisma. cofigure.ts
@@ -1,13 +1,1 @@
-// prisma.config.ts
-import { defineConfig } from '@prisma/config';
 
-export default defineConfig({
-  earlyAccess: true,
-  schema: {
-    kind: 'single',
-    filePath: 'prisma/schema.prisma',
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
