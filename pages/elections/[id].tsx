6b497f7020c4f3f9472493f5b41583e12f99b983
albertocen-prog <albertjesocen@gmail.com>
2026-09-24 13:13:18 +0300

    Update [id].tsx

--- pages/elections/[id].tsx
+++ pages/elections/[id].tsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 type BallotSummary = {
   id: string;
