11dd4d001fe4385120e1a395abfa0c50bd1f8596
albertocen-prog <albertjesocen@gmail.com>
2026-09-24 22:34:46 +0300

    Update open.ts

--- pages/api/elections/[id]/open.ts
+++ pages/api/elections/[id]/open.ts
@@ -63,4 +63,4 @@ const handler = async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
   }
 };
 
-export default requireRole('ELECTION_OFFICIAL', 'ADMIN')(handler);
+export default requireRole(['ELECTION_OFFICIAL', 'ADMIN'])(handler);
