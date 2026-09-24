4c20cc2f7977c0b8e615f237c74754154981a562
albertocen-prog <albertjesocen@gmail.com>
2026-09-24 17:59:23 +0300

    Update middleware.ts

--- lib/auth/middleware.ts
+++ lib/auth/middleware.ts
@@ -1,4 +1,4 @@
-import { NextApiResponse, NextApiRequest } from 'next';
+import { NextApiResponse } from 'next';
 import { NextApiRequestWithAuth, requireRole } from '@/lib/auth/middleware';
 import { prisma } from '@/lib/db';
 
