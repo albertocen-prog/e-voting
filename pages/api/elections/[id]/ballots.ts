25285fb1910d8e832f48020b98348072b0b3ec93
albertocen-prog <albertjesocen@gmail.com>
2026-09-24 22:17:24 +0300

    Update ballots.ts

--- pages/api/elections/[id]/ballots.ts
+++ pages/api/elections/[id]/ballots.ts
@@ -65,4 +65,4 @@ const handler = async (req: NextApiRequestWithAuth, res: NextApiResponse) => {
   }
 };
 
-export default requireRole('ELECTION_OFFICIAL', 'ADMIN')(handler);
+export default requireRole(['ELECTION_OFFICIAL', 'ADMIN'])(handler);
