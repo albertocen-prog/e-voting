53056634c0f7db0d778a201ecdc11beab314a589
albertocen-prog <albertjesocen@gmail.com>
2026-09-26 18:09:01 +0300

    Update login.tsx

--- pages/api/auth/login.tsx
+++ pages/api/auth/login.tsx
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '@/lib/db';
+import { FormEvent } from 'react';
 import { signToken } from '@/lib/auth/jwt';
 import { comparePassword } from '@/lib/auth/password';
 import { EmailLoginRequest, AuthResponse } from '@/lib/auth/types';
